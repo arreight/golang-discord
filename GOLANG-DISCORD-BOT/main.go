b82b27f322eeebd9437d696d6a7b054525ac2ecf
arreight <45423743+arreight@users.noreply.github.com>
2022-03-10 11:45:56 +0100

    Add files via upload

---
+++ GOLANG-DISCORD-BOT/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"golang-discord-bot/bot"
+	"golang-discord-bot/config"
+)
+
+func main() {
+	err := config.ReadConfig()
+	//!= is als er iets gebeurd wat niet verwerkt kan worden
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+
+	bot.Start()
+
+	<-make(chan struct{})
+	return
+}
